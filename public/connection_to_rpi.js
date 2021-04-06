5f7457e4bcec125a9c8eb295d81b09e7ee304593
Sardar Hassan <sardarhassan10@gmail.com>
2021-04-06 11:36:36 +0500

    Changed layout on small devices

--- public/connection_to_rpi.js
+++ public/connection_to_rpi.js
@@ -1,16 +1,20 @@
 // Create WebSocket connection.
 const socket = new WebSocket('ws://192.168.100.103:40000');
-  
+var connected = false;
 // Connection opened
 socket.addEventListener('open', function (event) {
     console.log('Connected to the WS Server!');
     document.getElementById("Status").innerHTML = "Status: Connected";
+    document.getElementById("Status").style.color = "#53a351";
+    connected = true;
 });
 
 // Connection closed
 socket.addEventListener('close', function (event) {
     console.log('Disconnected from the WS Server!');
     document.getElementById("Status").innerHTML = "Status: Disconnected. Please make sure RPI is on and refresh this page.";
+    document.getElementById("Status").style.color = "red";
+    connected = false;
 });
 
 // Listen for messages
@@ -19,6 +23,10 @@ socket.addEventListener('message', function (event) {
 });
 // Send a msg to the websocket
 const connect = () => {
-    socket.send('Start');
-    console.log('sent');
+    if (connected === false){
+        alert('No connection. Please make sure RPI is connected and then refresh this page.');
+    } else {
+        socket.send('Start');
+        console.log('sent');
+    }
 }
